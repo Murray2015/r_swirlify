8f73d0d243bc6d2f62226c4edcd5ec683d327708
Murray2015 <murrayhoggett@gmail.com>
2018-04-16 07:28:13 +0100

    first commit - part way through plotting basemaps

---
+++ course_outline.R
@@ -0,0 +1,17 @@
+setwd("~/Documents/scratch/r_swirlify")
+
+## R for climate scientists 
+
+# Plotting base maps 
+
+# Working with NetCDF files 
+
+# Plotting maps for NetCDF files 
+
+# Advanced plotting maps for NetCDF files 
+
+# Plotting time series data with base R (basic)
+
+# Plotting time series data with ggplot2 (intermediate)
+
+# Working with time series data 
