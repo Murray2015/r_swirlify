747ae2139f578df5154afabe0cab5ae57a8a9aa1
Murray Hoggett <murrayhoggett@gmail.com>
2018-04-18 20:21:23 +0100

    deleted Rhistory

--- course_outline.R
+++ course_outline.R
@@ -5,8 +5,19 @@ setwd("~/Documents/scratch/r_swirlify")
 # Plotting base maps 
 
 # Working with NetCDF files 
+- ncdf4 lib 
+- opening file connections
+- ncget_var
+- ncget_att
+- nc_close
 
 # Plotting maps for NetCDF files 
+- Early note that just plots as a grid, therefore not a "proper" map projections. 
+- image 
+- image.plot 
+- Adding titles, axes labels and colourbar labels
+- Adding a map overlay 
+- Plotting a difference map, with polar colour bar and balanced with the zlim= argument. 
 
 # Advanced plotting maps for NetCDF files 
 
